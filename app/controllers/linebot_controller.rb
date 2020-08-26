ac3cc9700a75c5343b4d4f7d52207093c669b34b
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-27 02:19:14 +0900

    変更

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -28,7 +28,7 @@ class LinebotController < ApplicationController
         "成功者になろうとするな。価値ある者になろうとせよ。-アルバート・アインシュタイン-",
         "ディズニーランドが完成することはない。想像力が世の中にある限り進化し続けるだろう。-ウォルト・ディズニー-",
         "成功とは、どん底に落ちた時、どれほど高く跳ね上がれるか、ということである。-ジョージ・パットン-[陸軍軍人]"].shuffle.first
-      elsif event.message['text'].include?("一成の名言")
+      elsif event.message['text'].include?("一成からめいちゃんへ")
         response = ["大好きだよ","愛してるよ","好きだよ"].shuffle.first
       else
         response = ["「名言」と打つと偉人の名言が見れます。元気を貰おう！"]
