e0dbe695705efd2da2ca465219f81f5826219891
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-26 23:14:16 +0900

    おみくじ

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -20,49 +20,56 @@ class LinebotController < ApplicationController
 
     events = client.parse_events_from(body)
 
-    events.each { |event|
+    events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
-          if event.message['text'].eql?('アンケート')
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+        client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+          if event.message['text'] =~ /おみくじ/
             message[:text] = 
-              ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle.first
-            # private内のtemplateメソッドを呼び出します。
-            # client.reply_message(event['replyToken'], template)
+                ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle.first
           end
         end
       end
-    }
+    end
 
-    head :ok
+    "OK"
   end
-
-  private
-
-  # def template
-  #   {
-  #     "type": "template",
-  #     "altText": "this is a confirm template",
-  #     "template": {
-  #         "type": "confirm",
-  #         "text": "今日のもくもく会は楽しいですか？",
-  #         "actions": [
-  #             {
-  #               "type": "message",
-  #               # Botから送られてきたメッセージに表示される文字列です。
-  #               "label": "楽しい",
-  #               # ボタンを押した時にBotに送られる文字列です。
-  #               "text": "楽しい"
-  #             },
-  #             {
-  #               "type": "message",
-  #               "label": "楽しくない",
-  #               "text": "楽しくない"
-  #             }
-  #         ]
-  #     }
-  #   }
-  # end
 end
+
+#   private
+
+#   def template
+#     {
+#       "type": "template",
+#       "altText": "this is a confirm template",
+#       "template": {
+#           "type": "confirm",
+#           "text": "今日のもくもく会は楽しいですか？",
+#           "actions": [
+#               {
+#                 "type": "message",
+#                 # Botから送られてきたメッセージに表示される文字列です。
+#                 "label": "楽しい",
+#                 # ボタンを押した時にBotに送られる文字列です。
+#                 "text": "楽しい"
+#               },
+#               {
+#                 "type": "message",
+#                 "label": "楽しくない",
+#                 "text": "楽しくない"
+#               }
+#           ]
+#       }
+#     }
+#   end
+# end
