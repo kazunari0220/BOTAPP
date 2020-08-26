1d072ab003054186b962bc6a6f29ee41ef2b0d8b
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-26 22:25:54 +0900

    修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -27,8 +27,10 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
           if event.message['text'].eql?('アンケート')
+            message[:text] = 
+              ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle.first
             # private内のtemplateメソッドを呼び出します。
-            client.reply_message(event['replyToken'], template)
+            # client.reply_message(event['replyToken'], template)
           end
         end
       end
@@ -39,30 +41,28 @@ class LinebotController < ApplicationController
 
   private
 
-  def template
-    message[:text] = 
-    ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle.first
-    # {
-    #   "type": "template",
-    #   "altText": "this is a confirm template",
-    #   "template": {
-    #       "type": "confirm",
-    #       "text": "今日のもくもく会は楽しいですか？",
-    #       "actions": [
-    #           {
-    #             "type": "message",
-    #             # Botから送られてきたメッセージに表示される文字列です。
-    #             "label": "楽しい",
-    #             # ボタンを押した時にBotに送られる文字列です。
-    #             "text": "楽しい"
-    #           },
-    #           {
-    #             "type": "message",
-    #             "label": "楽しくない",
-    #             "text": "楽しくない"
-    #           }
-    #       ]
-    #   }
-    # }
-  end
+  # def template
+  #   {
+  #     "type": "template",
+  #     "altText": "this is a confirm template",
+  #     "template": {
+  #         "type": "confirm",
+  #         "text": "今日のもくもく会は楽しいですか？",
+  #         "actions": [
+  #             {
+  #               "type": "message",
+  #               # Botから送られてきたメッセージに表示される文字列です。
+  #               "label": "楽しい",
+  #               # ボタンを押した時にBotに送られる文字列です。
+  #               "text": "楽しい"
+  #             },
+  #             {
+  #               "type": "message",
+  #               "label": "楽しくない",
+  #               "text": "楽しくない"
+  #             }
+  #         ]
+  #     }
+  #   }
+  # end
 end
