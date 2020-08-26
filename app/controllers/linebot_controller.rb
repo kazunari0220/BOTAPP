63704583beb8a47c1b27d5f11159d50048baaf9d
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-26 22:17:24 +0900

    おみくじ

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -40,27 +40,29 @@ class LinebotController < ApplicationController
   private
 
   def template
-    {
-      "type": "template",
-      "altText": "this is a confirm template",
-      "template": {
-          "type": "confirm",
-          "text": "今日のもくもく会は楽しいですか？",
-          "actions": [
-              {
-                "type": "message",
-                # Botから送られてきたメッセージに表示される文字列です。
-                "label": "楽しい",
-                # ボタンを押した時にBotに送られる文字列です。
-                "text": "楽しい"
-              },
-              {
-                "type": "message",
-                "label": "楽しくない",
-                "text": "楽しくない"
-              }
-          ]
-      }
-    }
+    message[:text] = 
+    ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle.first
+    # {
+    #   "type": "template",
+    #   "altText": "this is a confirm template",
+    #   "template": {
+    #       "type": "confirm",
+    #       "text": "今日のもくもく会は楽しいですか？",
+    #       "actions": [
+    #           {
+    #             "type": "message",
+    #             # Botから送られてきたメッセージに表示される文字列です。
+    #             "label": "楽しい",
+    #             # ボタンを押した時にBotに送られる文字列です。
+    #             "text": "楽しい"
+    #           },
+    #           {
+    #             "type": "message",
+    #             "label": "楽しくない",
+    #             "text": "楽しくない"
+    #           }
+    #       ]
+    #   }
+    # }
   end
 end
