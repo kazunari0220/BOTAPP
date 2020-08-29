effe02dfb4494d9ef4a221e880a29c7d3cd49c27
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-29 18:31:34 +0900

    修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -37,13 +37,37 @@ class LinebotController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
-            "type": "image",
-            "originalContentUrl": "https://example.com/original.jpg",
-            "previewImageUrl": "https://example.com/preview.jpg"
-          }
-          message = {
-            "type": "text",
-            "text": "Hello, world"
+            "type": "template",
+            "altText": "this is a image carousel template",
+            "template": {
+              "type": "image_carousel",
+              "columns": [
+                {
+                  "imageUrl": "https://example.com/bot/images/item1.jpg",
+                  "action": {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                  }
+                },
+                {
+                  "imageUrl": "https://example.com/bot/images/item2.jpg",
+                  "action": {
+                    "type": "message",
+                    "label": "Yes",
+                    "text": "yes"
+                  }
+                },
+                {
+                  "imageUrl": "https://example.com/bot/images/item3.jpg",
+                  "action": {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                  }
+                }
+              ]
+            }
           }
           client.reply_message(event['replyToken'], message)
         end
