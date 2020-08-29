406c8524b1a9cc8a343c71f3cf3e30c26db2a947
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-29 18:40:54 +0900

    修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -43,27 +43,11 @@ class LinebotController < ApplicationController
               "type": "image_carousel",
               "columns": [
                 {
-                  "imageUrl": "https://example.com/bot/images/item1.jpg",
-                  "action": {
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=111"
-                  }
-                },
-                {
                   "imageUrl": "https://example.com/bot/images/item2.jpg",
                   "action": {
                     "type": "message",
                     "label": "Yes",
                     "text": "yes"
-                  }
-                },
-                {
-                  "imageUrl": "https://example.com/bot/images/item3.jpg",
-                  "action": {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/222"
                   }
                 }
               ]
