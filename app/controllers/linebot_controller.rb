c68e152133883b7394c08239d4c3ac163da9c7c8
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-29 17:19:35 +0900

    インデント

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -48,26 +48,26 @@ class LinebotController < ApplicationController
               "title": "Menu",
               "text": "Please select",
               "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/123"
+              },
+              "actions": [
+                {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=123"
+                },
+                {
+                  "type": "postback",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=123"
+                },
+                {
                   "type": "uri",
                   "label": "View detail",
                   "uri": "http://example.com/page/123"
-              },
-              "actions": [
-                  {
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=123"
-                  },
-                  {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=123"
-                  },
-                  {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/123"
-                  }
+                }
               ]
             }
           }
