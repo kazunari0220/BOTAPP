09cbbec35cd7f7e74e57f9b86d37ed780534d8e2
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-29 16:00:44 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -41,9 +41,9 @@ class LinebotController < ApplicationController
           message = {
             type: 'image',
             originalContentUrl: 'https://example.com/original.jpg',
-            previewImageUrl: 'https://example.com/preview.jpg',
-            type: 'text',
-            text: response
+            previewImageUrl: 'https://example.com/preview.jpg'
+            # type: 'text',
+            # text: response
           }
           client.reply_message(event['replyToken'], message)
         end
